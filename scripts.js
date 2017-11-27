08fb532224ee603650f5c928289a9da1d7579a0e
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-11-27 20:59:30 +0100

    Add comments in scripts

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 var name = prompt('Enter your nickname');
-alert('Hello, ' + name);
+//alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+//Kolejny komentarz
+
+/*
+	I kolejny komentarz
+	do komentarza
+
+*/
